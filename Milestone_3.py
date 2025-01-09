1b4297224d4f2b5d5446943ed7fb7740d236a476
Your Name <you@example.com>
2025-01-09 16:01:52 +0000

    Update Milestone_3.py with new functionality

--- Milestone_3.py
+++ Milestone_3.py
@@ -1,8 +1,8 @@
 def check_guess(guess, word):
-    # Step 2: Convert the guess into lowercase
+    # Step 1: Convert the guess into lowercase
     guess = guess.lower()
 
-    # Step 3: Check if the guess is in the word
+    # Step 2: Check if the guess is in the word
     if guess in word:
         print(f"Good guess! {guess} is in the word.")
     else:
@@ -11,12 +11,12 @@ def check_guess(guess, word):
 
 def ask_for_input(word):
     while True:
-        # Step 2: Ask the user to guess a letter
+        # Step 3: Ask the user to guess a letter
         guess = input("Guess a letter: ")
 
-        # Step 3: Check if the guess is a single alphabetical character
+        # Step 4: Check if the guess is a single alphabetical character
         if guess.isalpha() and len(guess) == 1:
-            # Step 3.1: Call check_guess to check if the guess is in the word
+            # Step: Call check_guess to check if the guess is in the word
             check_guess(guess, word)
             break  # Exit the loop after a valid guess
         else:
@@ -27,5 +27,5 @@ def ask_for_input(word):
 # Example word to guess
 word = "python"
 
-# Step 4: Outside the function, call ask_for_input to test your code
+# Step 6: Outside the function, call ask_for_input to test your code
 ask_for_input(word)
